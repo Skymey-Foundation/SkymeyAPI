a7108f91d8426cd29bca736cb9670df7e85cd0c6
dotnetcsharp0x <dsotnikov1996@gmail.com>
2024-02-16 10:32:06 +0300

    Добавьте файлы проекта.

---
+++ SkymeyAPI/Controllers/CryptoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkymeyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoController : Controller
+    {
+        [HttpGet("Get_order")]
+        public string Get()
+        {
+            return "ok";
+        }
+    }
+}
